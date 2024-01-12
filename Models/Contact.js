de4c391c44f45eeae9cc38e1bbc926250f89b846
mzaouchi <mzaouchi@gmail.com>
2024-01-12 09:10:54 +0100

    first commit

---
+++ Models/Contact.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+const contactSchema = new mongoose.Schema(
+    {
+        name : String,
+        age : Number,
+        email : {type : String, required : true, unique : true }
+    }
+)
+
+module.exports = mongoose.model('kountact',contactSchema)
